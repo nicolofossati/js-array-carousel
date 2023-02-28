4fc8ba41e169633472215b62d7ffac7b31c52c6b
nicolo fossati <nicofossa01@gmail.com>
2023-02-28 17:40:02 +0100

    sono a rrivato a poco dalla risoluzione del bonus 2 ma devo staccare purtroppo

--- BONUS/BONUS 2/js/main.js
+++ BONUS/BONUS 2/js/main.js
@@ -1,6 +1,7 @@
 const imgArray = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'];
 const sliderDom = document.querySelector('#slider');
 let sliderString = "";
+let thumbnailsStringImg = "";
 
 for(let i=0; i < imgArray.length; i++){
     if(i==0){
@@ -8,15 +9,27 @@ for(let i=0; i < imgArray.length; i++){
     } else {
         sliderString += `<div class="img-container"><img src="${imgArray[i]}"></div>`;
     }
+
+    thumbnailsStringImg +=  `<div class="thumb-img"><div class="border-container"></div><img class="filter-img" src="${imgArray[i]}"></div>`
 }
 
-sliderString += `<div id="up" class="arrow"><i class="fa-solid fa-angle-up"></i></div> <div id="down" class="arrow"><i class="fa-solid fa-angle-down"></i></div>`;
+thumbnailsStringImg = "<div class=\"img-thumb-container\">"+thumbnailsStringImg+"</div>";
+
+
+sliderString += `<div class="container-thumbnails">
+                    <div class="thumb-contents">
+                        ${thumbnailsStringImg}
+                        <div id="up" class="arrow"><i class="fa-solid fa-angle-up"></i></div>
+                        <div id="down" class="arrow"><i class="fa-solid fa-angle-down"></i></div>
+                    </div>
+                </div>`;
 sliderDom.innerHTML = sliderString;
 
 const upArrowDom = document.querySelector('#up');
 const downArrowDom = document.querySelector('#down');
 
 const imgContainerDom = document.getElementsByClassName('img-container');
+const imgThumbContDom = document.getElementsByClassName('thumb-img');
 
 /* Posso modificare le classi a partire da imgContainerDom in questo modo:
     imgContainerDom[i].classList.add('className');
@@ -32,6 +45,8 @@ upArrowDom.addEventListener('click', function(){
         currentImage = imgContainerDom.length-1;
     }
     imgContainerDom[currentImage].classList.add('show');
+
+
 });
 
 downArrowDom.addEventListener('click', function(){
